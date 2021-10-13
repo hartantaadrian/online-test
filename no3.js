7a5d4ae96256b0d162ef13be51b9849a4c4a934c
Adrian Hartanta <hartantaadrian@ymail.com>
2021-10-13 17:38:20 +0700

    update no3 answer

--- no3.js
+++ no3.js
@@ -6,30 +6,7 @@ function findFirstStringInBracket(str) {
       if (wordsAfterFirstBracket) {
         wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
         let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
-        if (indexClosingBracketFound >= 0) {
-          return wordsAfterFirstBracket.substring(0, indexClosingBracketFound);
-        } else {
-          return "";
-        }
-      } else {
-        return "";
-      }
-    } else {
-      return "";
-    }
-  } else {
-    return "";
-  }
-}
-
-function findFirstStringInBracket(str) {
-  if (str.length > 0) {
-    let indexFirstBracketFound = str.indexOf("(");
-    if (indexFirstBracketFound >= 0) {
-      let wordsAfterFirstBracket = str.substr(indexFirstBracketFound);
-      if (wordsAfterFirstBracket) {
-        wordsAfterFirstBracket = wordsAfterFirstBracket.substr(1);
-        let indexClosingBracketFound = wordsAfterFirstBracket.indexOf(")");
+        console.log(indexClosingBracketFound);
         if (indexClosingBracketFound >= 0) {
           return wordsAfterFirstBracket.substring(0, indexClosingBracketFound);
         }
