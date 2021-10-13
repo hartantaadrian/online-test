5bf3f009887a06da0ca16832451ec321e84f4aa0
Adrian Hartanta <hartantaadrian@ymail.com>
2021-10-13 17:13:03 +0700

    merge with herokuserver

--- connection/db.js
+++ connection/db.js
@@ -2,18 +2,11 @@ var mysql = require("mysql2");
 
 var util = require("util");
 
-// var pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "P@ssw0rd.1",
-//   database: "bibit",
-// });
-
 var pool = mysql.createPool({
-  host: "us-cdbr-east-04.cleardb.com",
-  user: "b5c81dec16d988",
-  password: "6916e208",
-  database: "heroku_49d27630e877873",
+  host: "localhost",
+  user: "root",
+  password: "P@ssw0rd.1",
+  database: "bibit",
 });
 
 // Ping database to check for common exception errors.
